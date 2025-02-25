8e21a831ef848822a64580cbf092e8a5d10d6f6e
Finja777 <Finnolshan@gmail.com>
2025-02-25 13:48:21 -0500

    Initial Commit

---
+++ WeNeedAName2/WeNeedAName2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeNeedAName2App.swift
+//  WeNeedAName2
+//
+//  Created by Finn Friedman-Olshan (student LM) on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct WeNeedAName2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeNeedAName2Tests/WeNeedAName2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  WeNeedAName2Tests.swift
+//  WeNeedAName2Tests
+//
+//  Created by Finn Friedman-Olshan (student LM) on 2/25/25.
+//
+
+import Testing
+
+struct WeNeedAName2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
